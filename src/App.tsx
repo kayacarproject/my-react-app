ef8dd6fed65056f93f804463cd5c7a0a51ca90d8
kayacarproject <kayacarproject@gmail.com>
2023-09-12 15:53:12 +0530

    fdfdeded

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,40 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         {/* <p>
+//           Edit <code>src/App.tsx</code> and save to reload.
+//         </p> */}
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import Posts from "./components/Posts";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <img src={logo} className="App-logo" alt="logo" />
+        <h1>Sagar Radadiya </h1>
       </header>
+      <Posts />
     </div>
   );
 }
