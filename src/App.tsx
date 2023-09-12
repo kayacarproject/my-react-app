aace930a13800c03d58ea67499416e5d2e5b0728
kayacarproject <kayacarproject@gmail.com>
2023-09-12 16:02:29 +0530

    chng

--- src/App.tsx
+++ src/App.tsx
@@ -33,7 +33,7 @@ function App() {
     <div className="App">
       <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
-        <h1>Sagar Radadiya </h1>
+        <h1>Sagar Radadiya Flutter Developer</h1>
       </header>
       <Posts />
     </div>
